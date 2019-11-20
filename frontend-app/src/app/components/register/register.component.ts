7ad543f0dab2b46b1545ec5bbdffd024f75b7dca
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2019-11-20 15:38:07 +0100

    Url fixes

--- frontend-app/src/app/components/register/register.component.ts
+++ frontend-app/src/app/components/register/register.component.ts
@@ -1,56 +1,59 @@
-import { Component, OnInit } from "@angular/core";
-import { UserService } from "../../services/user.service";
-import { DialogComponent } from "../dialog/dialog.component";
-import { MatDialog } from "@angular/material";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { DialogComponent } from '../dialog/dialog.component';
+import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-register",
-  templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.css"]
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: [ './register.component.css' ]
 })
 export class RegisterComponent implements OnInit {
-  user: any = {
-    email: "",
-    password: "",
-    name: ""
-  };
+	user: any = {
+		email: '',
+		password: '',
+		name: ''
+	};
 
-  showSpinner: boolean = false;
+	showSpinner: boolean = false;
 
-  constructor(
-    private userService: UserService,
-    public dialog: MatDialog,
-    private router: Router
-  ) {}
+	constructor(private userService: UserService, public dialog: MatDialog, private router: Router) {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 
-  showDialog(result, message?) {
-    const dialogRef = this.dialog.open(DialogComponent, {
-      width: "400px",
-      data: {
-        action: "Register ",
-        name: this.user.fullName,
-        message: message,
-        result: result
-      }
-    });
-    this.showSpinner = false;
-    this.user = {};
-    this.router.navigate(["/login"]);
-  }
+	showDialog(result, message?) {
+		const dialogRef = this.dialog.open(DialogComponent, {
+			width: '400px',
+			data: {
+				action: 'Register ',
+				name: this.user.fullName,
+				message: message,
+				result: result
+			}
+		});
+		this.showSpinner = false;
+		this.user = {};
+		this.router.navigate([ '/login' ]);
+	}
 
-  register() {
-    this.showSpinner = true;
-    this.userService.registerUser(this.user).subscribe(
-      (res: any) => {
-        this.showDialog(true, res.message);
-      },
-      err => {
-        let message = err.error.err.message;
-        this.showDialog(false, message);
-      }
-    );
-  }
+	register() {
+		this.user.username = this.user.name;
+		this.user.database_url = 'http:192.168.0.2';
+		this.user.database_name = 'TEST';
+		this.user.database_port = '1443';
+		this.user.database_username = 'sa';
+		this.user.database_password = 'masterkey';
+		console.log(this.user);
+		this.showSpinner = true;
+		this.userService.registerUser(this.user).subscribe(
+			(res: any) => {
+				this.showDialog(true, res.message);
+			},
+			(err) => {
+				let message = err.error.err.message;
+				this.showDialog(false, message);
+			}
+		);
+	}
 }
--- frontend-app/src/app/services/user.service.ts
+++ frontend-app/src/app/services/user.service.ts
@@ -5,28 +5,34 @@ import { AppComponent } from '../app.component';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class UserService {
-    
-    constructor(private http: HttpClient, private router: Router) { }
+	constructor(private http: HttpClient, private router: Router) {}
 
-    registerUser(userData: any){
-        return this.http.post( AppComponent.BACKEND_URL + '/register', { 
-            email: userData.email,
-            password: userData.password,
-            name: userData.name});
-    }
+	registerUser(userData: any) {
+		return this.http.post(AppComponent.BACKEND_URL + '/register/user', {
+			email: userData.email,
+			password: userData.password,
+			name: userData.name,
+			username: userData.username,
+			database_url: userData.database_url,
+			database_name: userData.database_name,
+			database_port: userData.database_port,
+			database_username: userData.database_username,
+			database_password: userData.database_password
+		});
+	}
 
-    getUserById(){
-        let currentUser = localStorage.getItem('userID');
-        // params = params.append('_id', currentUser);
-        return this.http.get(`${AppComponent.BACKEND_URL}/user/${currentUser}`);
-    }
+	getUserById() {
+		let currentUser = localStorage.getItem('userID');
+		// params = params.append('_id', currentUser);
+		return this.http.get(`${AppComponent.BACKEND_URL}/user/${currentUser}`);
+	}
 
-    updateUser(user: any){
-        let currentUser = localStorage.getItem('userID');
-        // params = params.append('_id', currentUser);
-        return this.http.put(`${AppComponent.BACKEND_URL}/user/${currentUser}`, {user});
-    }
+	updateUser(user: any) {
+		let currentUser = localStorage.getItem('userID');
+		// params = params.append('_id', currentUser);
+		return this.http.put(`${AppComponent.BACKEND_URL}/user/${currentUser}`, { user });
+	}
 }
