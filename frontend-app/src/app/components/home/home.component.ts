1763fa2950bc86160f4d4fd6fc1b51e5973a65c7
gitadmin <gitadmin>
2019-11-21 16:13:03 +0100

    Test sql server connection

--- frontend-app/src/app/components/home/home.component.ts
+++ frontend-app/src/app/components/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../services/user.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: "app-home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+	constructor(public userService: UserService) {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+	test() {
+		this.userService.test().subscribe(response => {
+			console.log(response);
+		});
+	}
 }
