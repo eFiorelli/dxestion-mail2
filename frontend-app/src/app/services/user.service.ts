ee8d16d065fe92b9b74c78f8fb1d0bc0fa388727
gitadmin <gitadmin>
2019-11-25 17:36:11 +0100

    Nothing

--- frontend-app/src/app/services/user.service.ts
+++ frontend-app/src/app/services/user.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Router } from '@angular/router';
-import { AppComponent } from '../app.component';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { AppComponent } from "../app.component";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: "root"
 })
 export class UserService {
 	constructor(private http: HttpClient, private router: Router) {}
 
 	test() {
-		return this.http.post(AppComponent.BACKEND_URL + '/register/client', {
-			email: 'test8@test.com',
-			name: 'Test user',
-			phone: '666555444'
+		return this.http.post(AppComponent.BACKEND_URL + "/register/client", {
+			email: "test8@test.com",
+			name: "Test user",
+			phone: "666555444"
 		});
 	}
 
 	registerUser(userData: any) {
-		return this.http.post(AppComponent.BACKEND_URL + '/register/user', {
+		return this.http.post(AppComponent.BACKEND_URL + "/register/user", {
 			email: userData.email,
 			password: userData.password,
 			name: userData.name,
@@ -33,14 +33,17 @@ export class UserService {
 	}
 
 	getUserById() {
-		let currentUser = localStorage.getItem('userID');
+		let currentUser = localStorage.getItem("userID");
 		// params = params.append('_id', currentUser);
 		return this.http.get(`${AppComponent.BACKEND_URL}/user/${currentUser}`);
 	}
 
 	updateUser(user: any) {
-		let currentUser = localStorage.getItem('userID');
+		let currentUser = localStorage.getItem("userID");
 		// params = params.append('_id', currentUser);
-		return this.http.put(`${AppComponent.BACKEND_URL}/user/${currentUser}`, { user });
+		return this.http.put(
+			`${AppComponent.BACKEND_URL}/user/${currentUser}`,
+			{ user }
+		);
 	}
 }
