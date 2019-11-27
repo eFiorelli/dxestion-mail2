3abc805d542079030fafa86ad205486d92f71fc7
gitadmin <gitadmin>
2019-11-27 14:09:45 +0100

    Create uploads folder structure

--- backend/server/server.js
+++ backend/server/server.js
@@ -44,14 +44,31 @@ app.use(bodyParser.raw());
 // parse application/json
 app.use(bodyParser.json());
 
-// configuracion global de rutas
+/* Global route config */
 app.use(require('./routes/index'));
 
-// habilitar la carpeta public
+/* Enable public directory */
 app.use(express.static(path.resolve(__dirname, '../public/')));
 
+/* Create uploads folder tree */
+const uploads_dir = './uploads/';
+const background_dir = './uploads/background/';
+const background_logo = './uploads/logo/';
+
+if (!fs.existsSync(uploads_dir)) {
+	fs.mkdirSync(uploads_dir);
+}
+
+if (!fs.existsSync(background_dir)) {
+	fs.mkdirSync(background_dir);
+}
+
+if (!fs.existsSync(background_logo)) {
+	fs.mkdirSync(background_logo);
+}
+
 if (process.env.NODE_ENV === 'prod') {
-	setTimeout(function() {
+	setTimeout(function () {
 		mongoose.connect(process.env.URLDB, (err, res) => {
 			if (err) throw err;
 			console.log('Database....ONLINE');
