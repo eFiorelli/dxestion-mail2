c3fe73922a287fbb88c1e1d8baf80912a6043279
gitadmin <gitadmin>
2019-11-27 17:18:46 +0100

    New user images upload

--- frontend-app/src/app/components/register/register.component.ts
+++ frontend-app/src/app/components/register/register.component.ts
@@ -1,58 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user.service';
-import { DialogComponent } from '../dialog/dialog.component';
-import { MatDialog } from '@angular/material';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../services/user.service";
+import { DialogComponent } from "../dialog/dialog.component";
+import { MatDialog } from "@angular/material";
+import { Router } from "@angular/router";
 
 @Component({
-	selector: 'app-register',
-	templateUrl: './register.component.html',
-	styleUrls: [ './register.component.css' ]
+	selector: "app-register",
+	templateUrl: "./register.component.html",
+	styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
 	user: any = {
-		username: '',
-		password: '',
-		name: '',
-		email: '',
-		database_url: '',
-		database_name: '',
-		database_port: '',
-		database_username: '',
-		database_password: ''
+		username: "qqq",
+		password: "qqq",
+		name: "qqq",
+		email: "qqq@qqq.com",
+		database_url: "192.168.0.2",
+		database_name: "BD1",
+		database_port: "1443",
+		database_username: "sa",
+		database_password: "masterkey",
+		background_img: "",
+		logo_img: ""
 	};
 
 	showSpinner: boolean = false;
 
-	constructor(private userService: UserService, public dialog: MatDialog, private router: Router) {}
+	constructor(
+		private userService: UserService,
+		public dialog: MatDialog,
+		private router: Router
+	) {}
 
 	ngOnInit() {}
 
 	showDialog(result, message?) {
 		const dialogRef = this.dialog.open(DialogComponent, {
-			width: '400px',
+			width: "400px",
 			data: {
-				action: 'Register ',
+				action: "Register ",
 				name: this.user.fullName,
 				message: message,
 				result: result
 			}
 		});
 		this.showSpinner = false;
-		this.user = {};
-		this.router.navigate([ '/login' ]);
+		// this.user = {};
+		// this.router.navigate(["/login"]);
 	}
 
 	register() {
 		this.showSpinner = true;
-		this.userService.registerUser(this.user).subscribe(
-			(res: any) => {
-				this.showDialog(true, res.message);
-			},
-			(err) => {
-				let message = err.error.err.message;
-				this.showDialog(false, message);
-			}
-		);
+		// this.userService.registerUser(this.user).subscribe(
+		// 	(res: any) => {
+		// 		this.showDialog(true, res.message);
+		// 	},
+		// 	err => {
+		// 		let message = err.error.err.message;
+		// 		this.showDialog(false, message);
+		// 	}
+		// );
+		console.log(this.user.background_img);
+		if (this.userService.registerUser(this.user)) {
+			this.showDialog(true, "Success");
+		} else {
+			this.showDialog(false, "Error");
+		}
+	}
+
+	selectBGImage(event) {
+		this.user.background_img = event.target.files[0];
+	}
+
+	selectLogoImage(event) {
+		this.user.logo_img = event.target.files[0];
 	}
 }
--- frontend-app/src/app/services/user.service.ts
+++ frontend-app/src/app/services/user.service.ts
@@ -1,33 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Router } from '@angular/router';
-import { AppComponent } from '../app.component';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { AppComponent } from "../app.component";
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: "root"
 })
 export class UserService {
-	constructor(private http: HttpClient, private router: Router) { }
+	constructor(private http: HttpClient, private router: Router) {}
 
-	userID = localStorage.getItem('userID');
+	userID = localStorage.getItem("userID");
 
 	test() {
-		return this.http.post(AppComponent.BACKEND_URL + '/register/client', {
-			email: 'test8@test.com',
-			name: 'Test user',
-			phone: '666555444'
+		return this.http.post(AppComponent.BACKEND_URL + "/register/client", {
+			email: "test8@test.com",
+			name: "Test user",
+			phone: "666555444"
 		});
 	}
 
 	upload(file) {
 		return new Promise((resolve, reject) => {
-			console.log(file);
 			const formData = new FormData();
 			const xhr = new XMLHttpRequest();
-
-			formData.append('image', file, file.name);
-			xhr.onreadystatechange = function () {
+			formData.append("image", file, file.name);
+			xhr.onreadystatechange = function() {
 				if (xhr.readyState === 4) {
 					if (xhr.status === 200) {
 						resolve(JSON.parse(xhr.response));
@@ -36,8 +34,9 @@ export class UserService {
 					}
 				}
 			};
-			const url = AppComponent.BACKEND_URL + '/upload/logo/' + this.userID;
-			xhr.open('POST', url, true);
+			const url =
+				AppComponent.BACKEND_URL + "/upload/logo/" + this.userID;
+			xhr.open("POST", url, true);
 			xhr.send(formData);
 		});
 		//let url = AppComponent.BACKEND_URL + '/upload/background/5dda91afbd61a150e0313932';
@@ -45,7 +44,31 @@ export class UserService {
 	}
 
 	registerUser(userData: any) {
-		return this.http.post(AppComponent.BACKEND_URL + '/register/user', {
+		const bg_file = userData.background_img;
+		const logo_file = userData.logo_img;
+		return new Promise((resolve, reject) => {
+			const formData = new FormData();
+			const xhr = new XMLHttpRequest();
+			for (var key in userData) {
+				formData.append(key, userData[key]);
+			}
+			formData.append("background_image", bg_file, bg_file.name);
+			formData.append("logo_image", logo_file, logo_file.name);
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState === 4) {
+					if (xhr.status === 200) {
+						resolve(JSON.parse(xhr.response));
+					} else {
+						reject(JSON.parse(xhr.response));
+					}
+				}
+			};
+			const url = AppComponent.BACKEND_URL + "/register/user";
+			xhr.open("POST", url, true);
+			xhr.send(formData);
+		});
+		/*
+		return this.http.post(AppComponent.BACKEND_URL + "/register/user", {
 			email: userData.email,
 			password: userData.password,
 			name: userData.name,
@@ -54,19 +77,25 @@ export class UserService {
 			database_name: userData.database_name,
 			database_port: userData.database_port,
 			database_username: userData.database_username,
-			database_password: userData.database_password
+			database_password: userData.database_password,
+			background_img: userData.background_img,
+			logo_img: userData.logo_img
 		});
+		*/
 	}
 
 	getUserById() {
-		let currentUser = localStorage.getItem('userID');
+		let currentUser = localStorage.getItem("userID");
 		// params = params.append('_id', currentUser);
 		return this.http.get(`${AppComponent.BACKEND_URL}/user/${currentUser}`);
 	}
 
 	updateUser(user: any) {
-		let currentUser = localStorage.getItem('userID');
+		let currentUser = localStorage.getItem("userID");
 		// params = params.append('_id', currentUser);
-		return this.http.put(`${AppComponent.BACKEND_URL}/user/${currentUser}`, { user });
+		return this.http.put(
+			`${AppComponent.BACKEND_URL}/user/${currentUser}`,
+			{ user }
+		);
 	}
 }
