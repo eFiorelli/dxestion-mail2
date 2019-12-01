b4857e766b0687e111903fecc2174d8cb39c5390
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2019-12-01 16:35:06 +0100

    <feature> Update list user/store endpoints

--- backend/server/routes/store/list_stores.js
+++ backend/server/routes/store/list_stores.js
@@ -1,53 +1,85 @@
 const express = require('express');
 const Store = require('../../models/store');
+const User = require('../../models/user');
 const { checkUserToken, checkAdminRole, checkUserRole } = require('../../middlewares/authentication');
 const app = express();
 
-app.get('/stores', [ checkUserToken, checkAdminRole, checkUserRole ], (req, res) => {
-	Store.find(
-		{ active: true, user: req.query.user_id },
-		'_id name email username database_url database_name database_port database_username database_password logo_img'
-	).exec((err, stores) => {
-		if (err) {
+app.get('/stores', [ checkUserToken, checkUserRole ], async (req, res) => {
+	const user_id = req.query.user_id;
+	try {
+		const is_admin = await User.findById(user_id);
+		let query = '';
+		if (is_admin.role === 'ADMIN_ROLE') {
+			query = Store.find({});
+		} else {
+			query = Store.find({ active: true, user: req.query.user_id });
+			query.select(
+				'_id name email username database_url database_name database_port database_username database_password logo_img'
+			);
+		}
+
+		const stores = await query.exec();
+		if (!stores) {
 			return res.status(400).json({
 				ok: false,
 				err: err
 			});
 		}
 
-		Store.countDocuments({ active: true, user: req.user }, (err, count) => {
-			return res.status(200).json({
-				ok: true,
-				stores: stores,
-				count: count
-			});
+		const count = await Store.countDocuments({ active: true, user: req.user });
+		return res.status(200).json({
+			ok: true,
+			stores: stores,
+			count: count
 		});
-	});
+	} catch (err) {
+		return res.status(500).json({
+			ok: false,
+			err: err
+		});
+	}
 });
 
-app.get('/store/:id', [ checkUserToken, checkAdminRole ], (req, res) => {
+app.get('/store/:id', [ checkUserToken ], async (req, res) => {
 	let id = req.params.id;
+	let is_admin = false;
 
-	Store.findById(id, (err, storeDB) => {
-		if (err) {
-			return res.status(400).json({
+	try {
+		const user = await User.findById(req.user._id);
+		is_admin = user.role === 'ADMIN_ROLE';
+	} catch (err) {
+		return res.status(500).json({
+			ok: false,
+			err: err
+		});
+	}
+
+	if (id !== req.user._id && !is_admin) {
+		return res.status(400).json({
+			ok: false,
+			err: 'You are not allowed to view this store'
+		});
+	} else {
+		try {
+			const storeDB = await Store.findById(id);
+			if (storeDB) {
+				return res.status(200).json({
+					ok: true,
+					store: storeDB
+				});
+			} else {
+				return res.status(400).json({
+					ok: false,
+					message: 'Store not found'
+				});
+			}
+		} catch (err) {
+			return res.status(500).json({
 				ok: false,
 				err: err
 			});
 		}
-
-		if (!storeDB) {
-			return res.status(400).json({
-				ok: false,
-				message: 'Store not found'
-			});
-		} else {
-			return res.status(200).json({
-				ok: true,
-				store: storeDB
-			});
-		}
-	});
+	}
 });
 
 module.exports = app;
--- backend/server/routes/user/list_users.js
+++ backend/server/routes/user/list_users.js
@@ -3,51 +3,69 @@ const User = require('../../models/user');
 const { checkUserToken, checkAdminRole } = require('../../middlewares/authentication');
 const app = express();
 
-app.get('/users', [checkUserToken, checkAdminRole], (req, res) => {
-	User.find(
-		{ active: true, role: 'USER_ROLE' },
-		'_id name email username logo_img'
-	).exec((err, users) => {
-		if (err) {
+app.get('/users', [ checkUserToken, checkAdminRole ], async (req, res) => {
+	try {
+		const users = await User.find({ active: true, role: 'USER_ROLE' }, '_id name email username logo_img').exec();
+		if (!users) {
 			return res.status(400).json({
 				ok: false,
-				err: err
+				err: 'Error getting users'
 			});
-		}
-
-		User.countDocuments({ active: true, role: 'USER_ROLE' }, (err, count) => {
+		} else {
+			const count = await User.countDocuments({ active: true, role: 'USER_ROLE' });
 			return res.status(200).json({
 				ok: true,
 				users: users,
 				count: count
 			});
+		}
+	} catch (err) {
+		return res.status(500).json({
+			ok: false,
+			err: err
 		});
-	});
+	}
 });
 
-app.get('/user/:id', [checkUserToken, checkAdminRole], (req, res) => {
+app.get('/user/:id', [ checkUserToken ], async (req, res) => {
 	let id = req.params.id;
+	let is_admin = false;
+	try {
+		const user = await User.findById(req.user._id);
+		is_admin = user.role === 'ADMIN_ROLE';
+	} catch (err) {
+		return res.status(500).json({
+			ok: false,
+			err: err
+		});
+	}
 
-	User.findById(id, (err, userDB) => {
-		if (err) {
-			return res.status(400).json({
+	if (id !== req.user._id && !is_admin) {
+		return res.status(400).json({
+			ok: false,
+			err: 'You are not allowed to view this user'
+		});
+	} else {
+		try {
+			const userDB = await User.findById(id);
+			if (!userDB) {
+				return res.status(400).json({
+					ok: false,
+					message: 'User not found'
+				});
+			} else {
+				return res.status(200).json({
+					ok: true,
+					user: userDB
+				});
+			}
+		} catch (err) {
+			return res.status(500).json({
 				ok: false,
 				err: err
 			});
 		}
-
-		if (!userDB) {
-			return res.status(400).json({
-				ok: false,
-				message: 'User not found'
-			});
-		} else {
-			return res.status(200).json({
-				ok: true,
-				user: userDB
-			});
-		}
-	});
+	}
 });
 
 module.exports = app;
