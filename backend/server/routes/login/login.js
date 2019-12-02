34d89408e4d356b95665fb8bfdd2dea8071b9793
gitadmin <gitadmin>
2019-12-03 00:47:37 +0100

    <feature> Split login into users and stores

--- backend/server/routes/login/login.js
+++ backend/server/routes/login/login.js
@@ -3,10 +3,12 @@ const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken');
 const User = require('../../models/user');
+const Store = require('../../models/store');
 const app = express();
 
-app.post('/login', async (req, res) => {
-	let credentials = req.body.credentials;
+app.post('/login/:type', async (req, res) => {
+	const credentials = req.body.credentials;
+	const type = req.params.type;
 
 	if (credentials) {
 		username = credentials.username;
@@ -18,43 +20,18 @@ app.post('/login', async (req, res) => {
 			type: 21
 		});
 	}
-
 	try {
-		const userDB = await User.findOne({
-			username: username
-		});
-
-		if (userDB) {
-			if (!bcrypt.compareSync(password, userDB.password) && userDB.password !== password) {
-				return res.status(400).json({
-					ok: false,
-					message: 'Wrong username/password',
-					type: 22
-				});
-			}
-
-			let token = jwt.sign({ user: userDB }, process.env.SEED, { expiresIn: process.env.TOKEN_EXPIRATION });
-
-			let returnedUser = {
-				_id: userDB._id,
-				username: userDB.username,
-				name: userDB.name,
-				email: userDB.email,
-				role: userDB.role
-			};
-
-			return res.status(200).json({
-				ok: true,
-				user: returnedUser,
-				is_admin: userDB.role === 'ADMIN_ROLE',
-				token: token
+		if (type === 'user') {
+			const userDB = await User.findOne({
+				username: username
 			});
-		} else {
-			return res.status(400).json({
-				ok: false,
-				message: 'User not found',
-				type: 4
+			await userLogin(res, userDB, credentials);
+		}
+		if (type === 'store') {
+			const storeDB = await Store.findOne({
+				username: username
 			});
+			await storeLogin(res, storeDB, credentials);
 		}
 	} catch (err) {
 		return res.status(500).json({
@@ -64,5 +41,74 @@ app.post('/login', async (req, res) => {
 		});
 	}
 });
+
+
+userLogin = async (res, userDB, credentials) => {
+	if (userDB) {
+		if (!bcrypt.compareSync(credentials.password, userDB.password) && userDB.password !== credentials.password) {
+			return res.status(400).json({
+				ok: false,
+				message: 'Wrong username/password',
+				type: 22
+			});
+		}
+
+		const token = jwt.sign({ user: userDB }, process.env.SEED, { expiresIn: process.env.TOKEN_EXPIRATION });
+
+		const returnedUser = {
+			_id: userDB._id,
+			username: userDB.username,
+			name: userDB.name,
+			email: userDB.email,
+			role: userDB.role
+		};
+
+		return res.status(200).json({
+			ok: true,
+			user: returnedUser,
+			is_admin: userDB.role === 'ADMIN_ROLE',
+			token: token
+		});
+	} else {
+		return res.status(400).json({
+			ok: false,
+			message: 'User not found',
+			type: 4
+		});
+	}
+};
+
+storeLogin = async (res, storeDB, credentials) => {
+	if (storeDB) {
+		if (!bcrypt.compareSync(credentials.password, storeDB.password) && storeDB.password !== credentials.password) {
+			return res.status(400).json({
+				ok: false,
+				message: 'Wrong username/password',
+				type: 22
+			});
+		}
+
+		const token = jwt.sign({ store: storeDB }, process.env.SEED, { expiresIn: process.env.TOKEN_EXPIRATION });
+
+		const returnedStore = {
+			_id: storeDB._id,
+			username: storeDB.username,
+			name: storeDB.name,
+			email: storeDB.email,
+		};
+
+		return res.status(200).json({
+			ok: true,
+			store: returnedStore,
+			token: token
+		});
+	} else {
+		return res.status(400).json({
+			ok: false,
+			message: 'Store not found',
+			type: 4
+		});
+	}
+};
 
 module.exports = app;
