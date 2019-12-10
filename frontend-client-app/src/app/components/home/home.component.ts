9db17d1e4f06df1d3d64c87293bdb64033110811
gitadmin <gitadmin>
2019-12-10 16:57:49 +0100

    Minor design features

--- frontend-client-app/src/app/components/home/home.component.ts
+++ frontend-client-app/src/app/components/home/home.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { UserService } from '../../services/user.service';
-import { AppComponent } from '../../app.component';
-import Swal from 'sweetalert2';
-import { TranslateService } from '@ngx-translate/core';
-import { SignaturePad } from 'angular2-signaturepad/signature-pad';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { UserService } from "../../services/user.service";
+import { AppComponent } from "../../app.component";
+import Swal from "sweetalert2";
+import { TranslateService } from "@ngx-translate/core";
+import { SignaturePad } from "angular2-signaturepad/signature-pad";
 
 declare var $: any;
 
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.component.html',
-	styleUrls: [ './home.component.css' ]
+	selector: "app-home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
 	@ViewChild(SignaturePad) signaturePad: SignaturePad;
 
-	constructor(public userService: UserService, private translate: TranslateService) {}
+	constructor(
+		public userService: UserService,
+		private translate: TranslateService
+	) {}
 
 	selectedFile: any;
-	imagePath = AppComponent.BACKEND_URL + '/files/store/background/';
-	backgroundImg = '';
+	imagePath = AppComponent.BACKEND_URL + "/files/store/background/";
+	backgroundImg = "";
 
 	client = {
-		email: '',
-		name: '',
-		phone: '',
+		email: "",
+		name: "",
+		phone: "",
 		signature: new Blob(),
 		gpdr: false
 	};
@@ -34,14 +37,14 @@ export class HomeComponent implements OnInit {
 		minWidth: 5,
 		canvasWidth: 250,
 		canvasHeight: 200,
-		backgroundColor: 'white'
+		backgroundColor: "white"
 	};
 
 	ngAfterViewInit() {
 		// this.signaturePad is now available
-		this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
+		this.signaturePad.set("minWidth", 5); // set szimek/signature_pad options at runtime
 		this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
-		$('canvas').css('border-radius', '15px');
+		$("canvas").css("border-radius", "15px");
 	}
 
 	drawComplete() {
@@ -51,75 +54,91 @@ export class HomeComponent implements OnInit {
 
 	drawStart() {
 		// will be notified of szimek/signature_pad's onBegin event
-		console.log('begin drawing');
+		console.log("begin drawing");
 	}
 
 	ngOnInit() {
-		if (localStorage.getItem('bg_image')) {
-			this.backgroundImg = this.imagePath + localStorage.getItem('bg_image');
+		if (localStorage.getItem("bg_image") !== "undefined") {
+			this.backgroundImg =
+				this.imagePath + localStorage.getItem("bg_image");
 		} else {
-			this.backgroundImg = '../../../assets/bg-heading-03.jpg';
+			this.backgroundImg = "../../../assets/bg-heading-03.jpg";
 		}
 	}
 
 	registerClient() {
 		if (!this.client.gpdr) {
-			const gpdr_text = this.translate.instant('ERRORS.GPDR_ACCEPT');
-			Swal.fire('Error', gpdr_text, 'error');
+			const gpdr_text = this.translate.instant("ERRORS.GPDR_ACCEPT");
+			Swal.fire("Error", gpdr_text, "error");
 			return;
 		}
-		this.client.signature = this.dataURItoBlob(this.signaturePad.toDataURL('image/png'));
+		this.client.signature = this.dataURItoBlob(
+			this.signaturePad.toDataURL("image/png")
+		);
 		this.userService
 			.registerClient(this.client)
-			.then((response) => {
-				const success_text = 'Cliente creado con exito';
-				Swal.fire('Exito', success_text, 'success').then(() => {
+			.then(response => {
+				const success_text = "Cliente creado con exito";
+				Swal.fire("Exito", success_text, "success").then(() => {
 					this.client = {
-						email: '',
-						name: '',
-						phone: '',
+						email: "",
+						name: "",
+						phone: "",
 						signature: new Blob(),
 						gpdr: false
 					};
 					this.signaturePad.clear();
 				});
 			})
-			.catch((error) => {
-				const error_text = this.translate.instant(`ERRORS.ERROR_TYPE_${error.type}`);
-				Swal.fire('Error', error_text, 'error');
+			.catch(error => {
+				const error_text = this.translate.instant(
+					`ERRORS.ERROR_TYPE_${error.type}`
+				);
+				Swal.fire("Error", error_text, "error");
 			});
 	}
 
 	randomClient() {
-		let random = Math.random().toString(36).substring(2, 15);
-		let randomPhone = Math.floor(Math.random() * (699999999 - 600000000 + 1)) + 600000000;
+		let random = Math.random()
+			.toString(36)
+			.substring(2, 15);
+		let randomPhone =
+			Math.floor(Math.random() * (699999999 - 600000000 + 1)) + 600000000;
 		let client = {
 			email: `${random}@${random}.com`,
 			name: random,
 			phone: randomPhone,
 			signature: new Blob()
 		};
-		client.signature = this.dataURItoBlob(this.signaturePad.toDataURL('image/png'));
+		client.signature = this.dataURItoBlob(
+			this.signaturePad.toDataURL("image/png")
+		);
 		this.userService
 			.registerClient(client)
-			.then((response) => {
-				const success_text = 'Cliente creado con exito';
-				Swal.fire('Exito', success_text, 'success');
+			.then(response => {
+				const success_text = "Cliente creado con exito";
+				Swal.fire("Exito", success_text, "success");
 			})
-			.catch((error) => {
-				const error_text = this.translate.instant(`ERRORS.ERROR_TYPE_${error.type}`);
-				Swal.fire('Error', error_text, 'error');
+			.catch(error => {
+				const error_text = this.translate.instant(
+					`ERRORS.ERROR_TYPE_${error.type}`
+				);
+				Swal.fire("Error", error_text, "error");
 			});
 	}
 
 	dataURItoBlob(dataURI) {
 		// convert base64/URLEncoded data component to raw binary data held in a string
 		var byteString;
-		if (dataURI.split(',')[0].indexOf('base64') >= 0) byteString = atob(dataURI.split(',')[1]);
-		else byteString = unescape(dataURI.split(',')[1]);
+		if (dataURI.split(",")[0].indexOf("base64") >= 0)
+			byteString = atob(dataURI.split(",")[1]);
+		else byteString = unescape(dataURI.split(",")[1]);
 
 		// separate out the mime component
-		var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+		var mimeString = dataURI
+			.split(",")[0]
+			.split(":")[1]
+			.split(";")[0];
 
 		// write the bytes of the string to a typed array
 		var ia = new Uint8Array(byteString.length);
@@ -127,6 +146,6 @@ export class HomeComponent implements OnInit {
 			ia[i] = byteString.charCodeAt(i);
 		}
 
-		return new Blob([ ia ], { type: mimeString });
+		return new Blob([ia], { type: mimeString });
 	}
 }
--- frontend-client-app/src/app/components/login/login.component.ts
+++ frontend-client-app/src/app/components/login/login.component.ts
@@ -1,35 +1,37 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material';
-import { AuthService } from '../../services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
+import Swal from "sweetalert2";
 
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: [ './login.component.css' ]
+	selector: "app-login",
+	templateUrl: "./login.component.html",
+	styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
-	constructor(private auth: AuthService, private router: Router, public snackBar: MatSnackBar) {}
+export class LoginComponent implements OnInit {
+	constructor(private auth: AuthService, private router: Router) {}
 
-	username: string = 'b02f1uo4eb4';
-	password: string = '1234';
+	username: string = "igavd24chk";
+	password: string = "1234";
 	credentials: Object;
 	showSpinner: boolean;
+
+	ngOnInit() {
+		localStorage.clear();
+	}
 
 	login(): void {
 		this.showSpinner = true;
 		this.credentials = { username: this.username, password: this.password };
 		this.auth.login(this.credentials).subscribe(
-			(res) => {
+			res => {
 				if (res) {
 					this.showSpinner = false;
-					this.router.navigate([ '/home' ]);
+					this.router.navigate(["/home"]);
 				}
 			},
-			(error) => {
-				this.snackBar.open('Incorrect login', 'dismiss', {
-					duration: 3000
-				});
+			error => {
+				Swal.fire("Login incorrecto", error, "error");
 				this.showSpinner = false;
 			}
 		);
