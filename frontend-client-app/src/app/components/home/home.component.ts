0bb18af666c86ef180c129d95bb1d0b643cfa9ca
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2019-12-12 21:55:09 +0100

    <bugfix> Fix file format

--- frontend-client-app/src/app/components/home/home.component.ts
+++ frontend-client-app/src/app/components/home/home.component.ts
@@ -10,15 +10,12 @@ declare var $: any;
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
-	styleUrls: ['./home.component.css']
+	styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent implements OnInit {
 	@ViewChild(SignaturePad) signaturePad: SignaturePad;
 
-	constructor(
-		public userService: UserService,
-		private translate: TranslateService
-	) {}
+	constructor(public userService: UserService, private translate: TranslateService) {}
 
 	selectedFile: any;
 	imagePath = AppComponent.BACKEND_URL + '/files/store/background/';
@@ -50,18 +47,15 @@ export class HomeComponent implements OnInit {
 
 	drawComplete() {
 		// will be notified of szimek/signature_pad's onEnd event
-		// console.log(this.signaturePad.toDataURL('image/png'));
 	}
 
 	drawStart() {
 		// will be notified of szimek/signature_pad's onBegin event
-		console.log('begin drawing');
 	}
 
 	ngOnInit() {
 		if (localStorage.getItem('bg_image') !== 'undefined') {
-			this.backgroundImg =
-				this.imagePath + localStorage.getItem('bg_image');
+			this.backgroundImg = this.imagePath + localStorage.getItem('bg_image');
 		} else {
 			this.backgroundImg = '../../../assets/bg-heading-03.jpg';
 		}
@@ -73,21 +67,17 @@ export class HomeComponent implements OnInit {
 			Swal.fire('Error', gpdr_text, 'error');
 			return;
 		}
-		this.client.signature = this.dataURItoBlob(
-			this.signaturePad.toDataURL('image/png')
-		);
+		this.client.signature = this.dataURItoBlob(this.signaturePad.toDataURL('image/png'));
 		this.userService
 			.registerClient(this.client)
-			.then(response => {
+			.then((response) => {
 				const success_text = 'Cliente creado con exito';
 				Swal.fire('Exito', success_text, 'success').then(() => {
 					this.flip();
 				});
 			})
-			.catch(error => {
-				const error_text = this.translate.instant(
-					`ERRORS.ERROR_TYPE_${error.type}`
-				);
+			.catch((error) => {
+				const error_text = this.translate.instant(`ERRORS.ERROR_TYPE_${error.type}`);
 				Swal.fire('Error', error_text, 'error');
 			});
 	}
@@ -116,30 +106,23 @@ export class HomeComponent implements OnInit {
 	}
 
 	randomClient() {
-		const random = Math.random()
-			.toString(36)
-			.substring(2, 15);
-		const randomPhone =
-			Math.floor(Math.random() * (699999999 - 600000000 + 1)) + 600000000;
+		const random = Math.random().toString(36).substring(2, 15);
+		const randomPhone = Math.floor(Math.random() * (699999999 - 600000000 + 1)) + 600000000;
 		const client = {
 			email: `${random}@${random}.com`,
 			name: random,
 			phone: randomPhone,
 			signature: new Blob()
 		};
-		client.signature = this.dataURItoBlob(
-			this.signaturePad.toDataURL('image/png')
-		);
+		client.signature = this.dataURItoBlob(this.signaturePad.toDataURL('image/png'));
 		this.userService
 			.registerClient(client)
-			.then(response => {
+			.then((response) => {
 				const success_text = 'Cliente creado con exito';
 				Swal.fire('Exito', success_text, 'success');
 			})
-			.catch(error => {
-				const error_text = this.translate.instant(
-					`ERRORS.ERROR_TYPE_${error.type}`
-				);
+			.catch((error) => {
+				const error_text = this.translate.instant(`ERRORS.ERROR_TYPE_${error.type}`);
 				Swal.fire('Error', error_text, 'error');
 			});
 	}
@@ -154,10 +137,7 @@ export class HomeComponent implements OnInit {
 		}
 
 		// separate out the mime component
-		const mimeString = dataURI
-			.split(',')[0]
-			.split(':')[1]
-			.split(';')[0];
+		const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
 		// write the bytes of the string to a typed array
 		const ia = new Uint8Array(byteString.length);
@@ -165,6 +145,6 @@ export class HomeComponent implements OnInit {
 			ia[i] = byteString.charCodeAt(i);
 		}
 
-		return new Blob([ia], { type: mimeString });
+		return new Blob([ ia ], { type: mimeString });
 	}
 }
