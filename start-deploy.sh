509d8858db77496ac7b379556ccbe39f17a96300
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2019-12-22 14:25:16 +0100

    <bugfix> Fix docker settings

--- start-deploy.sh
+++ start-deploy.sh
@@ -5,9 +5,13 @@ echo "Removing docker containers...."
 docker-compose down -v
 rm -r traefik
 
-# Remove docker network
-echo "Removing docker network"
+# Remove web network
+echo "Removing web network"
 docker network remove web
+
+# Remove database network
+echo "Removing database network"
+docker network remove database
 
 # Remove backend server folder
 echo "Removing backend folder..."
@@ -18,9 +22,13 @@ echo "Removing nginx config files..."
 rm -r nginx_conf
 rm default.conf
 
-# Remove frontend app folder
-echo "Removing frontend folder..."
-rm -r frontend-app
+# Remove frontend admin app folder
+echo "Removing frontend admin folder..."
+rm -r frontend-admin-app
+
+# Remove frontend client app folder
+echo "Removing frontend client folder..."
+rm -r frontend-client-app
 
 #Remove traefik files and folders
 echo "Removing traefik stuff..."
@@ -36,20 +44,20 @@ rm -rf readme.MD
 rm -rf .git
 rm -rf .gitignore
 git init
-git remote add origin http://git.serantes.pro/gitadmin/dxestion-mail.git
+git remote add origin https://git.serantes.pro/gitadmin/dxestion-mail.git
 git pull origin master
 
 # Set permissions to SSL certs acme file
 echo "Setting 600 to acme.json..."
 chmod 600 traefik/acme.json
 
-# Create docker external network
-echo "Creating docker external network..."
-docker network create web
-
 # Create docker internal network
 echo "Creating docker internal network..."
 docker network create database
+
+# Create docker external network
+echo "Creating docker external network..."
+docker network create web
 
 # Start deploy
 echo "Starting deploy..."
