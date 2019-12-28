64446e32fbda2a9e8be023b3f230d209de2d2ef6
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2019-12-28 21:06:50 +0100

    <bugfix> Minor fixes

--- frontend-admin-app/src/app/components/navbar/navbar.component.ts
+++ frontend-admin-app/src/app/components/navbar/navbar.component.ts
@@ -3,12 +3,11 @@ import { AuthService } from '../../services/auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Location } from '@angular/common';
-import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html',
-	styleUrls: ['./navbar.component.css']
+	styleUrls: [ './navbar.component.css' ]
 })
 export class NavbarComponent implements OnInit {
 	activeLang = 'es';
@@ -17,17 +16,12 @@ export class NavbarComponent implements OnInit {
 		public auth: AuthService,
 		public router: Router,
 		private translate: TranslateService,
-		public location: Location,
-		public ls: LocalStorageService
+		public location: Location
 	) {
 		this.translate.setDefaultLang(this.activeLang);
 	}
 
-	ngOnInit() {
-		this.ls.watchStorage().subscribe((data: string) => {
-			console.log('changeeee');
-		});
-	}
+	ngOnInit() {}
 
 	changeLanguage(lang: string) {
 		this.activeLang = lang;
@@ -35,34 +29,30 @@ export class NavbarComponent implements OnInit {
 	}
 
 	registerUser() {
-		this.router.navigate(['/register']);
+		this.router.navigate([ '/register' ]);
 	}
 
 	userProfile() {
-		this.router.navigate(['/profile']);
+		this.router.navigate([ '/profile' ]);
 	}
 
 	users() {
-		this.router.navigate(['/users']);
+		this.router.navigate([ '/users' ]);
 	}
 
 	stores() {
-		this.router.navigate(['/stores']);
+		this.router.navigate([ '/stores' ]);
 	}
 
 	home() {
-		this.router.navigate(['/home']);
+		this.router.navigate([ '/home' ]);
 	}
 
 	login() {
-		this.router.navigate(['/login']);
+		this.router.navigate([ '/login' ]);
 	}
 
 	logout() {
 		this.auth.logout();
-	}
-
-	test() {
-		localStorage.setItem('test', '1');
 	}
 }
