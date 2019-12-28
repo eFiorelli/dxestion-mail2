e482808dc213f9e4c56ebd5688869df07f6821b8
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2019-12-28 21:04:32 +0100

    <bugfix> Fix register store issues

--- backend/server/routes/store/register_store.js
+++ backend/server/routes/store/register_store.js
@@ -1,100 +1,92 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-let {
-	checkUserToken,
-	checkUserRole,
-	checkAdminRole
-} = require("../../middlewares/authentication");
-const Store = require("../../models/store");
+const express = require('express');
+const bcrypt = require('bcrypt');
+let { checkUserToken, checkUserRole, checkAdminRole } = require('../../middlewares/authentication');
+const Store = require('../../models/store');
 const app = express();
 
-app.post(
-	"/register/store",
-	[checkUserToken, checkAdminRole, checkUserRole],
-	async (req, res) => {
-		let body = req.body;
+app.post('/register/store', [ checkUserToken, checkAdminRole, checkUserRole ], async (req, res) => {
+	let body = req.body;
 
-		try {
-			const storeDB = await Store.findOne({
-				username: body.username
+	try {
+		const storeDB = await Store.findOne({
+			username: body.username
+		});
+		if (storeDB) {
+			return res.status(400).json({
+				ok: false,
+				message: 'There already exists an store with this name',
+				type: 9
 			});
-			if (storeDB) {
-				return res.status(400).json({
-					ok: false,
-					message: "There already exists an store with this name",
-					type: 9
-				});
-			} else {
-				let store = new Store({
-					name: body.name,
-					email: body.email,
-					password: bcrypt.hashSync(body.password, 10),
-					username: body.username,
-					database_url: body.database_url,
-					database_name: body.database_name,
-					database_port: body.database_port,
-					database_username: body.database_username,
-					database_password: body.database_password,
-					store_type: body.store_type,
-					commerce_password: body.commerce_password,
-					user: body.user
-				});
+		} else {
+			let store = new Store({
+				name: body.name,
+				email: body.email,
+				password: bcrypt.hashSync(body.password, 10),
+				username: body.username,
+				database_url: body.database_url,
+				database_name: body.database_name,
+				database_port: body.database_port,
+				database_username: body.database_username,
+				database_password: body.database_password,
+				store_type: body.store_type,
+				commerce_password: body.commerce_password,
+				user: body.user
+			});
 
-				const savedStore = await store.save();
-				if (savedStore) {
-					if (req.files) {
-						const images = [
-							{
-								type: "background_1",
-								image: req.files.background_img_1
-							},
-							{
-								type: "background_2",
-								image: req.files.background_img_2
-							},
-							{
-								type: "background_3",
-								image: req.files.background_img_3
-							},
-							{
-								type: "background_4",
-								image: req.files.background_img_4
-							},
-							{
-								type: "background_5",
-								image: req.files.background_img_5
-							},
-							{
-								type: "logo",
-								image: req.files.logo_image
-							}
-						];
-						await saveStoreImages(savedStore._id, res, images);
-					} else {
-						return res.status(200).json({
-							ok: true,
-							message: "Store successfully created",
-							store: savedStore,
-							type: 1
-						});
-					}
+			const savedStore = await store.save();
+			if (savedStore) {
+				if (req.files) {
+					const images = [
+						{
+							type: 'background_1',
+							image: req.files.background_img_1
+						},
+						{
+							type: 'background_2',
+							image: req.files.background_img_2
+						},
+						{
+							type: 'background_3',
+							image: req.files.background_img_3
+						},
+						{
+							type: 'background_4',
+							image: req.files.background_img_4
+						},
+						{
+							type: 'background_5',
+							image: req.files.background_img_5
+						},
+						{
+							type: 'logo',
+							image: req.files.logo_image
+						}
+					];
+					await saveStoreImages(savedStore._id, res, images);
 				} else {
-					return res.status(400).json({
-						ok: false,
-						message: "Failed on creating store",
-						type: 10
+					return res.status(200).json({
+						ok: true,
+						message: 'Store successfully created',
+						store: savedStore,
+						type: 1
 					});
 				}
+			} else {
+				return res.status(400).json({
+					ok: false,
+					message: 'Failed on creating store',
+					type: 10
+				});
 			}
-		} catch (err) {
-			return res.status(500).json({
-				ok: false,
-				err: err,
-				type: 1
-			});
 		}
+	} catch (err) {
+		return res.status(500).json({
+			ok: false,
+			err: err,
+			type: 1
+		});
 	}
-);
+});
 
 saveStoreImages = async (id, res, images) => {
 	try {
@@ -102,7 +94,7 @@ saveStoreImages = async (id, res, images) => {
 		if (!storeDB) {
 			return res.status(400).json({
 				ok: false,
-				message: "Store not found",
+				message: 'Store not found',
 				type: 11
 			});
 		} else {
@@ -110,31 +102,32 @@ saveStoreImages = async (id, res, images) => {
 				let file = images[i].image;
 				if (file) {
 					// Valid extensions
-					let validExtensions = ["png", "jpg", "gif", "jpeg"];
-					let shortedName = file.name.split(".");
+					let validExtensions = [ 'png', 'jpg', 'gif', 'jpeg' ];
+					let shortedName = file.name.split('.');
 					let extension = shortedName[shortedName.length - 1];
 
 					if (validExtensions.indexOf(extension) < 0) {
 						return res.status(400).json({
 							ok: false,
-							meesage:
-								"Allowed extensions: " +
-								validExtensions.join(", ")
+							meesage: 'Allowed extensions: ' + validExtensions.join(', ')
 						});
 					}
 
 					// Change filename
-					let filename = `${id}-${new Date().getMilliseconds()}.${extension}`;
+					let filename = `${storeDB._id}-${new Date().getMilliseconds() * Math.random()}.${extension}`;
 
+					let type = '';
 					// Use the mv() method to place the file somewhere on your server
-					if (images[i].type === `background_${i}`) {
-						storeDB[`background_img_${i}`] = `${filename}`;
+					if (images[i].type === `background_${i + 1}`) {
+						storeDB.background_img[i] = filename;
+						type = 'background';
 					}
-					if (images[i].type === "logo") {
-						storeDB.logo_img = `${filename}`;
+					if (images[i].type === 'logo') {
+						storeDB.logo_img = filename;
+						type = 'logo';
 					}
 
-					file.mv(`uploads/${images[i].type}/${filename}`, err => {
+					file.mv(`uploads/store/${type}/${filename}`, (err) => {
 						if (err) {
 							return res.status(500).json({
 								ok: false,
@@ -145,11 +138,14 @@ saveStoreImages = async (id, res, images) => {
 					});
 				}
 			}
-			const updatedStore = await storeDB.save();
+			const updatedStore = await Store.findByIdAndUpdate(storeDB._id, {
+				background_img: storeDB.background_img,
+				logo_image: storeDB.logo_image
+			});
 			if (updatedStore) {
 				return res.status(200).json({
 					ok: true,
-					message: "Store successfully created",
+					message: 'Store successfully created',
 					store: updatedStore,
 					type: 2
 				});
--- backend/server/routes/store/update_store.js
+++ backend/server/routes/store/update_store.js
@@ -1,161 +1,145 @@
-const path = require("path");
-const fs = require("fs");
-const express = require("express");
-const bcrypt = require("bcrypt");
-let {
-	checkUserToken,
-	checkAdminRole,
-	checkUserRole
-} = require("../../middlewares/authentication");
-const Store = require("../../models/store");
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const bcrypt = require('bcrypt');
+let { checkUserToken, checkAdminRole, checkUserRole } = require('../../middlewares/authentication');
+const Store = require('../../models/store');
 const app = express();
 
-app.put(
-	"/update/store/:id",
-	[checkUserToken, checkAdminRole, checkUserRole],
-	async (req, res) => {
-		let body = req.body;
-		let id = req.params.id;
+app.put('/update/store/:id', [ checkUserToken, checkAdminRole, checkUserRole ], async (req, res) => {
+	let body = req.body;
+	let id = req.params.id;
 
-		try {
-			const storeDB = await Store.findById(id);
-			if (!body.password) {
-				await Store.update({
-					id: body._id,
-					name: body.name,
-					email: body.email,
-					username: body.username,
-					database_url: body.database_url,
-					database_name: body.database_name,
-					database_port: body.database_port,
-					database_username: body.database_username,
-					database_password: body.database_password,
-					user: body.user
-				});
-			} else {
-				await Store.update({
-					id: body._id,
-					name: body.name,
-					email: body.email,
-					password: bcrypt.hashSync(body.password, 10),
-					username: body.username,
-					database_url: body.database_url,
-					database_name: body.database_name,
-					database_port: body.database_port,
-					database_username: body.database_username,
-					database_password: body.database_password,
-					user: body.user
-				});
-			}
-
-			if (!storeDB) {
-				return res.status(400).json({
-					ok: false,
-					message: "There is no store with that ID",
-					type: 12
-				});
-			} else {
-				if (req.files) {
-					const images = [
-						{
-							type: "background_1",
-							image: req.files.background_img_1
-						},
-						{
-							type: "background_2",
-							image: req.files.background_img_2
-						},
-						{
-							type: "background_3",
-							image: req.files.background_img_3
-						},
-						{
-							type: "background_4",
-							image: req.files.background_img_4
-						},
-						{
-							type: "background_5",
-							image: req.files.background_img_5
-						},
-						{
-							type: "logo",
-							image: req.files.logo_image || ""
-						}
-					];
-					await updateStoreImages(storeDB, res, images);
-				} else {
-					return res.status(200).json({
-						ok: true,
-						message: "User updated successfully",
-						store: storeDB,
-						type: 1
-					});
-				}
-			}
-		} catch (err) {
-			return res.status(500).json({
-				ok: false,
-				err: err,
-				type: 1
+	try {
+		const storeDB = await Store.findById(id);
+		if (!body.password) {
+			await Store.findByIdAndUpdate(storeDB._id, {
+				id: body._id,
+				name: body.name,
+				email: body.email,
+				username: body.username,
+				database_url: body.database_url,
+				database_name: body.database_name,
+				database_port: body.database_port,
+				database_username: body.database_username,
+				database_password: body.database_password,
+				user: body.user
+			});
+		} else {
+			await Store.findByIdAndUpdate(storeDB._id, {
+				id: body._id,
+				name: body.name,
+				email: body.email,
+				password: bcrypt.hashSync(body.password, 10),
+				username: body.username,
+				database_url: body.database_url,
+				database_name: body.database_name,
+				database_port: body.database_port,
+				database_username: body.database_username,
+				database_password: body.database_password,
+				user: body.user
 			});
 		}
+
+		if (!storeDB) {
+			return res.status(400).json({
+				ok: false,
+				message: 'There is no store with that ID',
+				type: 12
+			});
+		} else {
+			if (req.files) {
+				const images = [
+					{
+						type: 'background_1',
+						image: req.files.background_img_1
+					},
+					{
+						type: 'background_2',
+						image: req.files.background_img_2
+					},
+					{
+						type: 'background_3',
+						image: req.files.background_img_3
+					},
+					{
+						type: 'background_4',
+						image: req.files.background_img_4
+					},
+					{
+						type: 'background_5',
+						image: req.files.background_img_5
+					},
+					{
+						type: 'logo',
+						image: req.files.logo_image || ''
+					}
+				];
+				await updateStoreImages(storeDB, res, images);
+			} else {
+				return res.status(200).json({
+					ok: true,
+					message: 'User updated successfully',
+					store: storeDB,
+					type: 1
+				});
+			}
+		}
+	} catch (err) {
+		return res.status(500).json({
+			ok: false,
+			err: err,
+			type: 1
+		});
 	}
-);
+});
 
 updateStoreImages = async (storeDB, res, images) => {
 	try {
 		if (!storeDB) {
 			return res.status(400).json({
 				ok: false,
-				message: "Store not found",
+				message: 'Store not found',
 				type: 11
 			});
 		} else {
 			for (let i = 0; i < images.length; i++) {
 				let file = images[i].image;
-
 				if (file) {
 					// Valid extensions
-					let validExtensions = ["png", "jpg", "gif", "jpeg"];
-					let shortedName = file.name.split(".");
+					let validExtensions = [ 'png', 'jpg', 'gif', 'jpeg' ];
+					let shortedName = file.name.split('.');
 					let extension = shortedName[shortedName.length - 1];
 
 					if (validExtensions.indexOf(extension) < 0) {
 						return res.status(400).json({
 							ok: false,
-							meesage:
-								"Allowed extensions: " +
-								validExtensions.join(", ")
+							meesage: 'Allowed extensions: ' + validExtensions.join(', ')
 						});
 					}
 
 					// Change filename
-					let filename = `${
-						storeDB._id
-					}-${new Date().getMilliseconds()}.${extension}`;
+					let filename = `${storeDB._id}-${new Date().getMilliseconds() * Math.random()}.${extension}`;
 
 					// Use the mv() method to place the file somewhere on your server
-					const oldFilenames = [
-						storeDB[`background_img_${i + 1}`],
-						storeDB.logo_img
-					];
+					const oldFilenames = [ storeDB.background_img[i], storeDB.logo_img ];
 
-					let type = "";
+					let type = '';
 					if (images[i].type === `background_${i + 1}`) {
-						if (storeDB[`background_img_${i + 1}`]) {
-							deleteStoreFiles("background", oldFilenames[0]);
+						if (storeDB.background_img[i]) {
+							deleteStoreFiles('background', oldFilenames[0]);
 						}
-						storeDB[`background_img_${i + 1}`] = `${filename}`;
-						type = "background";
+						storeDB.background_img[i] = filename;
+						type = 'background';
 					}
-					if (images[i].type === "logo") {
+					if (images[i].type === 'logo') {
 						if (storeDB.logo_img) {
-							deleteStoreFiles("logo", oldFilenames[1]);
+							deleteStoreFiles('logo', oldFilenames[1]);
 						}
-						storeDB.logo_img = `${filename}`;
-						type = "logo";
+						storeDB.logo_img = filename;
+						type = 'logo';
 					}
-					file.mv(`uploads/store/${type}/${filename}`, err => {
+					file.mv(`uploads/store/${type}/${filename}`, (err) => {
 						if (err) {
 							return res.status(500).json({
 								ok: false,
@@ -167,30 +151,21 @@ updateStoreImages = async (storeDB, res, images) => {
 				}
 			}
 
-			let bg_img = [];
-			bg_img.push(storeDB.background_img_1);
-			bg_img.push(storeDB.background_img_2);
-			bg_img.push(storeDB.background_img_3);
-			bg_img.push(storeDB.background_img_4);
-			bg_img.push(storeDB.background_img_5);
-
 			const updatedStore = await Store.findByIdAndUpdate(storeDB._id, {
-				background_img: bg_img,
+				background_img: storeDB.background_img,
 				logo_image: storeDB.logo_image
 			});
-			console.log(updatedStore);
 			if (updatedStore) {
 				updatedStore.save();
 				return res.status(200).json({
 					ok: true,
-					message: "User updated successfully",
+					message: 'User updated successfully',
 					store: updatedStore,
 					type: 2
 				});
 			}
 		}
 	} catch (err) {
-		console.log(err);
 		return res.status(500).json({
 			ok: false,
 			err: err,
@@ -200,7 +175,7 @@ updateStoreImages = async (storeDB, res, images) => {
 };
 
 deleteStoreFiles = (type, filename) => {
-	let imagePath = path.resolve(`uploads/${type}/${filename}`);
+	let imagePath = path.resolve(`uploads/store/${type}/${filename}`);
 	if (fs.existsSync(imagePath)) {
 		fs.unlinkSync(imagePath);
 	}
