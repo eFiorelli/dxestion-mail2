f03d03874d27b7827db59fa7db556ada8fc76428
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2020-01-10 20:02:44 +0100

    <bugfix> Fix import path

--- frontend-client-app/src/app/components/slider/slider.component.ts
+++ frontend-client-app/src/app/components/slider/slider.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AppComponent } from '../../../../../frontend-admin-app/src/app/app.component';
+import { AppComponent } from '../../app.component';
 
 declare var $: any;
 
