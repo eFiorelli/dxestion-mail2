5c0cb23c0bd162eb963cde1c5fc15dabf5f57618
Miguel Fuentes <miguel.fuentes.sobrin@gmail.com>
2021-06-17 13:34:57 +0200

    remove port redirection

--- frontend-admin-app/src/app/app.component.ts
+++ frontend-admin-app/src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'frontend-admin-app';
-	static BACKEND_URL = 'https://backend.nuclient.es:85';
-	static SOCKET_URL = 'https://backend.nuclient.es:85';
+	static BACKEND_URL = 'https://backend.nuclient.es';
+	static SOCKET_URL = 'https://backend.nuclient.es';
 	// static BACKEND_URL = 'http://localhost:3000';
 	// static SOCKET_URL = 'http://localhost:3000';
 }
